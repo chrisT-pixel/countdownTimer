7c01a0a90ae9ed1bfaa8d0aeffa9f271c97f8be7
Gabrus, Fedor - gabfy003 <gabfy003@mymail.unisa.edu.au>
2022-03-20 14:47:28 +1030

    Initial set up

---
+++ src/components/views/main-view.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+export function MainView() {
+    return(
+        <Container>
+            <p>This is where everything goes in.</p>
+        </Container>
+    );
+}

141d06538e1b0a57eecf1dc2f81515cd6070588e
Taylor, Chris - taycr003 <taycr003@mymail.unisa.edu.au>
2022-03-23 23:14:19 +1030

    begin sub task timer component with ability to create in mainView

---
+++ src/components/subtasks.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { useState } from 'react';
+import ReactDOM from "react-dom";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button';
+
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
+
+type Props = {
+  remainingTime: number
+}
+
+
+const renderTime = ({ remainingTime }: Props) => {
+  if (remainingTime === 0) {
+    return <div className="timer">Too late...</div>;
+  }
+
+  return (
+
+          <div className="timer">
+            <div className="text">Remaining</div>
+            <div className="value">{remainingTime}</div>
+            <div className="text">seconds</div>
+          </div>
+
+
+  );
+};
+
+export function Subtasks() {
+
+
+  return (
+
+      <Row className="subtask-row">
+
+        <Col>
+          <Form.Control placeholder="Sub Task Name" />
+        </Col>
+
+        <Col>
+          <div className="timer-wrapper">
+            <CountdownCircleTimer
+              isPlaying={true}
+              duration={60}
+              colors={["#004777", "#F7B801", "#A30000", "#A30000"]}
+              colorsTime={[10, 6, 3, 0]}
+              onComplete={() => ({ shouldRepeat: true, delay: 1 })}
+            >
+              {renderTime}
+            </CountdownCircleTimer>
+          </div>
+        </Col>
+
+        <Col>
+          <Button variant="danger">Delete (does nothing)</Button>
+        </Col>
+
+      </Row>
+
+
+  );
+}
--- src/components/views/main-view.tsx
+++ src/components/views/main-view.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { useState } from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import { Subtasks } from '../subtasks';
 
 export function MainView() {
+
+const [timers, setTimers] = useState([15]);
+
+ function addTimer() {
+
+   setTimers([...timers, 15])
+
+ }
     return(
         <Container>
-            <p>This is where everything goes in.</p>
+
+            <div className="App">
+              <Button variant="primary" onClick={addTimer}>Add New Subtask</Button>
+              <br />
+                {timers.map((item, i) => ( <Subtasks remainingTime={item} /> ))}
+            </div>
+
         </Container>
     );
 }
